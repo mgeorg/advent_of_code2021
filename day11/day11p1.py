2870f5de0778df6130971f9d77be1a5c7115f529
Manfred Georg <mgeorg@google.com>
2021-12-14 09:00:13 -0800

    solved day11 part 1

---
+++ day11/day11p1.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+import re
+import numpy as np
+import collections
+
+with open('input.txt', 'r') as f:
+  lines = f.read().splitlines()
+
+kMaxRange = 10
+
+class Board(object):
+  def __init__(self, lines):
+    self.board = list()
+    for line in lines:
+      self.board.append([int(x) for x in line.strip()])
+      assert len(self.board[-1]) == kMaxRange
+    assert len(self.board) == kMaxRange
+
+  def Iterate(self):
+    for i, row in enumerate(self.board):
+      for j, val in enumerate(row):
+        self.board[i][j] += 1
+
+    changed = True
+    flashed_count = 0
+    while changed:
+      changed = False
+      for i, row in enumerate(self.board):
+        for j, val in enumerate(row):
+          if val > 9:
+            self.board[i][j] = 0
+            flashed_count += 1
+            changed = True
+            for i_offset in [-1, 0, 1]:
+              for j_offset in [-1, 0, 1]:
+                if (i + i_offset >= 0 and j + j_offset >= 0 and
+                    i + i_offset < kMaxRange and j + j_offset < kMaxRange):
+                  if self.board[i+i_offset][j+j_offset] != 0:
+                    self.board[i+i_offset][j+j_offset] += 1
+    return flashed_count
+
+  def __str__(self):
+    return repr(self)
+
+  def __repr__(self):
+    output = ''
+    for row in self.board:
+      output += '  \'' + ''.join([str(x) for x in row]) + '\'\n'
+    return f'Board(\n{output})'
+
+b = Board(lines)
+print(b)
+total_flashes = 0
+for num in range(100):
+  total_flashes += b.Iterate()
+  print(num)
+  print(b)
+
+
+print(total_flashes)
